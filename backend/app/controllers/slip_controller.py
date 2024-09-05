232d22767c402a3203129310fbfe56195d92f09a
Marcio Tavares <marcio.tavares@bemobi.com>
2024-09-05 07:01:47 -0300

    Update

--- backend/app/controllers/slip_controller.py
+++ backend/app/controllers/slip_controller.py
@@ -10,7 +10,7 @@ slip_bp = Blueprint('slip', __name__)
 
 @slip_bp.route('/slips', methods=['POST'])
 @jwt_required()
-def create_slip_endpoint():
+def create_slip():
     data = request.get_json()
     current_user = get_jwt_identity()
     user = User.query.filter_by(username=current_user).first()
@@ -27,6 +27,7 @@ def create_slip_endpoint():
     return jsonify({
         "id": str(new_slip.id),
         "due_date": new_slip.due_date.strftime('%Y-%m-%d %H:%M:%S'),
+        "payment_date": new_slip.payment_date.strftime('%Y-%m-%d %H:%M:%S') if new_slip.payment_date else None,
         "value": new_slip.value,
         "description": new_slip.description,
         "status": new_slip.status.value
@@ -42,8 +43,8 @@ def get_slips():
 
     return jsonify([{
         "id": str(slip.id),
-        "appointment_date": slip.appointment_date.strftime('%Y-%m-%d %H:%M:%S') if slip.appointment_date else None,
         "due_date": slip.due_date.strftime('%Y-%m-%d %H:%M:%S'),
+        "payment_date": slip.payment_date.strftime('%Y-%m-%d %H:%M:%S') if slip.payment_date else None,
         "value": slip.value,
         "description": slip.description,
         "status": slip.status.value
@@ -51,16 +52,18 @@ def get_slips():
 
 @slip_bp.route('/slips/<slip_id>', methods=['PUT'])
 @jwt_required()
-def update_slip_endpoint(slip_id):
+def update_slip(slip_id):
     data = request.get_json()
 
     due_date = data.get('due_date')
+    payment_date = data.get('payment_date')
     value = data.get('value')
     description = data.get('description')
 
     updated_slip = update_slip(
         slip_id,
         due_date,
+        payment_date,
         value,
         description
     )
@@ -69,6 +72,7 @@ def update_slip_endpoint(slip_id):
         return jsonify({
             "id": str(updated_slip.id),
             "due_date": updated_slip.due_date.strftime('%Y-%m-%d %H:%M:%S'),
+            "payment_date": updated_slip.payment_date.strftime('%Y-%m-%d %H:%M:%S') if updated_slip.payment_date else None,
             "value": updated_slip.value,
             "description": updated_slip.description,
             "status": updated_slip.status.value
@@ -78,7 +82,7 @@ def update_slip_endpoint(slip_id):
 
 @slip_bp.route('/slips/<slip_id>', methods=['DELETE'])
 @jwt_required()
-def cancel_slip_endpoint(slip_id):
+def cancel_slip(slip_id):
     canceled_slip = cancel_slip(slip_id)
 
     if canceled_slip:
--- backend/app/services/slip_service.py
+++ backend/app/services/slip_service.py
@@ -2,13 +2,13 @@ from app import db
 from app.models.slip import Slip, SlipStatus
 from datetime import datetime
 
-def create_slip(user_id, due_date, value, description=None):
+def create_slip(user_id, value, description=None):
     new_slip = Slip(
         user_id=user_id,
-        due_date=due_date,
         value=value,
         description=description,
-        status=SlipStatus.PENDING
+        status=SlipStatus.PENDING,
+        due_date=datetime.now()
     )
     db.session.add(new_slip)
     db.session.commit()
@@ -17,20 +17,37 @@ def create_slip(user_id, due_date, value, description=None):
 def get_slips_by_user(user_id):
     return Slip.query.filter_by(user_id=user_id).all()
 
-def update_slip(slip_id, due_date, value, description=None):
+def update_slip(slip_id, due_date=None, payment_date=None, value=None, description=None):
     slip = Slip.query.get(slip_id)
+    
     if slip:
-        slip.due_date = datetime.strptime(due_date, '%Y-%m-%d')
-        slip.value = value
-        slip.description = description
+        if due_date:
+            try:
+                slip.due_date = datetime.strptime(due_date, '%Y-%m-%d')
+            except ValueError:
+                raise ValueError("Formato de data de vencimento inválido. Use 'YYYY-MM-DD'.")
+        
+        if payment_date:
+            try:
+                slip.payment_date = datetime.strptime(payment_date, '%Y-%m-%d')
+                slip.status = SlipStatus.PAID
+            except ValueError:
+                raise ValueError("Formato de data de pagamento inválido. Use 'YYYY-MM-DD'.")
+        
+        slip.value = value if value is not None else slip.value
+        slip.description = description if description is not None else slip.description
+        
         db.session.commit()
         return slip
+    
     return None
 
 def cancel_slip(slip_id):
     slip = Slip.query.get(slip_id)
+    
     if slip:
         slip.status = SlipStatus.CANCELLED
         db.session.commit()
         return slip
+    
     return None
